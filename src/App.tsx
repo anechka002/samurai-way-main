993e627b7fa07b12121b1e5d350cd07860383de0
Anna <blinova.anneta@gmail.com>
2024-03-26 12:35:40 +0100

    add Header and Technologies components to the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Technologies from './components/Technologies';
+
+export type TaskPropsType = {
+  id: number
+  title: string
+}
+export type TitlePropsType = {
+  id: number
+  title: string
+}
 
 function App() {
+  const task: Array<TaskPropsType> = [
+    {id: 1, title: 'css'},
+    {id: 2, title: 'html'},
+    {id: 3, title: 'js'},
+    {id: 4, title: 'react'},
+  ]
+  const title: Array<TitlePropsType> = [
+    {id: 1, title: '-Home- '},
+    {id: 2, title: '-News Feed- '},
+    {id: 3, title: '-Messages- '},
+  ]
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+      <Header titles={title}/>
+      <Technologies tasks={task}/>
     </div>
   );
 }
