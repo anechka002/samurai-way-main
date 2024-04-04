489867cadb7bbba3ebf392c59af40b8cb54db78f
Anna <blinova.anneta@gmail.com>
2024-04-04 10:47:23 +0200

    created Dialogs component and style.css

---
+++ src/components/dialogs/Dialogs.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './Dialogs.module.css'
+
+function Dialogs() {
+  return (
+    <div className={s.dialogs}>
+      
+    </div>
+  )
+}
+
+export default Dialogs
