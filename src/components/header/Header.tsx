1a20ce857769e0f58487c4751ef3110d9eac4527
Anna <blinova.anneta@gmail.com>
2024-04-04 10:46:56 +0200

    created Header components and style.css

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import s from './Header.module.css'
+
+function Header() {
+  return (
+    <header className={s.header}>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT58cCGmmFdKjaaU_otro1uki2TonktrPrTYg&usqp=CAU"
+        alt=""
+      />
+    </header>
+  );
+}
+
+export default Header;
