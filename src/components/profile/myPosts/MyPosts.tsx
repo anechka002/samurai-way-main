87f05b079a756c3f0c0c328d7802d00a7f6f7c8f
Anna <blinova.anneta@gmail.com>
2024-04-08 14:14:01 +0200

    added data

--- src/components/profile/myPosts/MyPosts.tsx
+++ src/components/profile/myPosts/MyPosts.tsx
@@ -2,20 +2,50 @@ import React, { useState } from 'react'
 import s from './MyPosts.module.css'
 import Post from './post/Post';
 
-function MyPosts() {
-  const [likeCount, setLikeCount] = useState(0)
-  console.log('likeCount: ', likeCount)
+export type PostType = {
+  id: number
+  img: string
+  message: string
+  likesCount: number
+}
 
-  const onClickLikeCount = (likeCount: number) => {
-    setLikeCount(likeCount + 1)
-  }
+function MyPosts() {
+  const [posts, setPosts] = useState<PostType[]>([
+      {
+        id: 1, 
+        img: "https://cs14.pikabu.ru/post_img/big/2023/02/13/8/1676295806139337963.png", 
+        message: 'Hi, how are you?',
+        likesCount: 10
+      },
+      {
+        id: 2, 
+        img: "https://cs14.pikabu.ru/post_img/big/2023/02/13/8/1676295806139337963.png", 
+        message: "I'm learn React",
+        likesCount: 0
+      },
+    ]
+  ) 
+
+
+const handleIncrementLikesCount = (postId: number) => {
+  setPosts(posts.map(p => p.id === postId ? {...p, likesCount: p.likesCount + 1} : p))
+}
 
   return (
     <div>
       <div>My post</div>
       <div>New post</div>
-      <Post onClickLikeCount={()=>onClickLikeCount(likeCount)} message='Hi, how are you?' likeCount={likeCount} />
-      {/* <Post message="I'm learn React" likeCount='15'/> */}
+      <input type="text" />
+      <button>add</button>
+
+      {posts.map((post) => (
+        <Post 
+        key={post.id} 
+        post={post} 
+        handleIncrementLikesCount={handleIncrementLikesCount}
+        />
+      ))}
+
     </div>
   )
 }
