bc1acd44fc7e656a808d622dd28762afbcb82b4b
Anna <blinova.anneta@gmail.com>
2024-04-08 14:11:05 +0200

    added routers

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import './App.css';
 import Header from './components/header/Header';
 import Profile from './components/profile/Profile';
 import NavBar from './components/navBar/NavBar';
+import Dialogs from './components/dialogs/Dialogs';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header/>
-      <NavBar/>
-      <Profile/>
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header/>
+        <NavBar/>
+        <div className='app-wrapper-content'>
+          <Route path={'/dialogs'} component={Dialogs}/>
+          <Route path={'/profile'} component={Profile}/>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
